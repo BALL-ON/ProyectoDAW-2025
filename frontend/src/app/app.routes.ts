e75fc3fc0b50d8c8934790bbd92fa8e860123582
Pedro Escudero Fernández <pedroescfer10@gmail.com>
2026-04-27 19:07:04 +0200

    FEAT - Primeros pasos creando los primeros componentes y cimentando la estructura

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './components/inicio/inicio';
+import { Faq } from './components/faq/faq';
+import { Login } from './components/login/login';
+import { Registro } from './components/registro/registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Inicio },
+  { path: 'faq', component: Faq },
+  { path: 'login', component: Login },
+  { path: 'login/registro', component: Registro }
+];
