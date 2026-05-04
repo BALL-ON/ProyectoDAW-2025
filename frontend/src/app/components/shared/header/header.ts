5de999418a3cd305b477bd1bd05465c91ed2ef2e
Pedro Escudero Fernández <pedroescfer10@gmail.com>
2026-05-04 12:44:17 +0200

    FEAT - Desarrollado el Header (a falta de redirección de enlaces)

--- frontend/src/app/components/shared/header/header.ts
+++ frontend/src/app/components/shared/header/header.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-
+activeRoute = signal<string>('inicio');
+  menuOpen = signal<boolean>(false);
+ 
+  setActive(route: string): void {
+    this.activeRoute.set(route);
+    this.menuOpen.set(false); // Cierra el menú móvil al navegar
+  }
+ 
+  toggleMenu(): void {
+    this.menuOpen.set(!this.menuOpen());
+  }
 }
