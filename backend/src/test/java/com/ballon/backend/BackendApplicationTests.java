f62f1b7709a1681f3532dbfd53e4910f944bd3b1
Pedro Escudero Fernández <pedroescfer10@gmail.com>
2026-05-17 13:26:00 +0200

    WIP - Primera parte configuración de despliegue

--- backend/src/test/java/com/ballon/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/ballon/backend/BackendApplicationTests.java
@@ -2,9 +2,15 @@ package com.ballon.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.ballon.backend.services.EmailService;
 
 @SpringBootTest
 class BackendApplicationTests {
+	
+	@MockBean
+    private EmailService emailService;
 
 	@Test
 	void contextLoads() {
--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'reserva/:idPista',
+    renderMode: RenderMode.Server // O RenderMode.Client
+  },
+  {
+    path: 'polideportivos/:id/pistas',
+    renderMode: RenderMode.Server // o RenderMode.Client
+  },
+  {
+    path: 'pago/:idReserva',
+    renderMode: RenderMode.Server // o RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
